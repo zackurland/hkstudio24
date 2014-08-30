407fe072154913ad6c723cc2ed27239c5677646e
PaulJovanovic <paulrealsane@gmail.com>
2014-08-29 19:12:27 -0700

    Login to admin app

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,12 @@
 class AdminController < ActionController::Base
   # before you do admin things, you must be an admin...
   # is the user an admin? no? well get the fuck out!
+  before_action :authenticate_user!
+  before_action :authorize
+
+  def authorize
+    unless current_user.admin?
+      redirect_to root_path
+    end
+  end
 end

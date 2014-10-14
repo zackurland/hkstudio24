fc1c42efe0ba985ab0e57a8f97605b13c18096bf
PaulJovanovic <paulrealsane@gmail.com>
2014-10-14 14:52:51 -0700

    Contact email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,10 @@
 class Contact < MailForm::Base
-  attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message,   :validate => true
-  attribute :company,   :validate => true
   attribute :to,        :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :name,      :validate => true
+  attribute :company,   :validate => true
   attribute :website
+  attribute :message,   :validate => true
 
   def headers
     {

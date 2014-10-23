5759f8fb8a5dbd4c9a464c55ca2c977cc3c02ad2
PaulJovanovic <paulrealsane@gmail.com>
2014-10-23 10:05:03 -0700

    CC Helene on emails sent

--- app/mailers/rental_mailer.rb
+++ app/mailers/rental_mailer.rb
@@ -3,13 +3,13 @@ class RentalMailer < ActionMailer::Base
 
   def invoice(rental)
     @rental = rental
-    mail(to: @rental.contact_email, subject: "HK STUDIO Invoice ##{@rental.id + 1000}")
+    mail(to: @rental.contact_email, cc: Editable.where(name: "Email").last.text, subject: "HK STUDIO Invoice ##{@rental.id + 1000}")
   end
 
   def agreement(rental, pdf_url)
     @rental = rental
     pdf = open(pdf_url)
     attachments["hkstudio_rental_agreement_#{1000 + @rental.id}.pdf"] = pdf.read
-    mail(to: @rental.contact_email, subject: "HK STUDIO Rental Agreement")
+    mail(to: @rental.contact_email, cc: Editable.where(name: "Email").last.text, subject: "HK STUDIO Rental Agreement")
   end
 end

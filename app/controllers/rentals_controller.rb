a273fff587ae21e5bc4aa72dc0dc494ea028bc28
PaulJovanovic <paulrealsane@gmail.com>
2014-10-22 18:50:20 -0700

    Bugs

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -18,6 +18,6 @@ class RentalsController < ApplicationController
   def rental_params
     params.require(:rental).permit(:status, :start_date, :end_date, :production_name, :production_company, :billing_name, :billing_address, :billing_address_2, :billing_city,
       :billing_state, :billing_country, :billing_zip_code, :designer_name, :designer_phone_number, :designer_email, :your_name,
-      :your_phone_number, :your_email, :filled_out_by_designer)
+      :your_phone_number, :your_email, :filled_out_by_designer, :duration_weeks)
   end
 end

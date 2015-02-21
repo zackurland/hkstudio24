1b917998fa75d4641a1b236bbf23acb6a6b642e2
superjova <paul@superjova.com>
2015-02-21 12:04:54 -0800

    Uniq product ids

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,10 +4,11 @@ class ProductsController < ApplicationController
     @tag_ids = []
     if params[:q].present?
       @tag_ids += params[:q].values.flatten
-      product_ids = Product.active.joins(:tags).where(tags: {id: @tag_ids}).group(:product_id).count.keep_if{|k, v| v >= params[:q].count}.keys
-      @products = Product.includes(:tags).where(products: { id: product_ids}).order(:display_index).paginate(page: params[:page], per_page: 20)
+      product_ids = Product.active.joins(:tags).where(tags: {id: @tag_ids}).group(:product_id).count.keep_if{|k, v| v >= params[:q].count}.keys.uniq
+      @products = Product.includes(:tags).where(products: { id: product_ids})
     else
-      @products = Product.active.includes(:tags).order(:display_index).paginate(page: params[:page], per_page: 20)
+      @products = Product.active.includes(:tags)
     end
+    @products = @products.order(:display_index).paginate(page: params[:page], per_page: 20)
   end
 end

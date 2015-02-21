8b8d40916c0d59826c084e422c91eb545d6421ab
superjova <paul@superjova.com>
2015-02-21 12:03:46 -0800

    Trying to fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,10 +5,9 @@ class ProductsController < ApplicationController
     if params[:q].present?
       @tag_ids += params[:q].values.flatten
       product_ids = Product.active.joins(:tags).where(tags: {id: @tag_ids}).group(:product_id).count.keep_if{|k, v| v >= params[:q].count}.keys
-      @products = Product.includes(:tags).where(products: { id: product_ids})
+      @products = Product.includes(:tags).where(products: { id: product_ids}).order(:display_index).paginate(page: params[:page], per_page: 20)
     else
-      @products = Product.active.includes(:tags)
+      @products = Product.active.includes(:tags).order(:display_index).paginate(page: params[:page], per_page: 20)
     end
-    @products = @products.order(:display_index).paginate(page: params[:page], per_page: 20)
   end
 end

cd801d36b75f5e22c4338e77f40911b52bda8022
superjova <paul@superjova.com>
2015-07-28 14:59:13 -0700

    Remove stuff

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,4 +1,10 @@
 class Admin::TagsController < AdminController
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to admin_category_path(@tag.category)
+  end
+
   def reorder
     params[:sortable].each_with_index do |id, index|
       Tag.find(id).update_column(:display_index, index + 1)

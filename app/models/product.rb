c291d53e4dd47d1a8df29f1127b2dddf132020ec
superjova <paul@superjova.com>
2015-08-25 16:42:27 -0700

    Correct filtering

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,13 +14,25 @@ class Product < ActiveRecord::Base
 
   before_create :set_display_index
 
-  product_ids = Product.joins(:tags).where(tags: {id: @tag_ids}).group(:product_id).count.keep_if{|k, v| v >= params[:q].count}.keys
-  @products = Product.includes(:tags).where(products: { id: product_ids}).uniq
-
   def self.filter(tag_hash)
+    category_ids = tag_hash.keys
     tag_ids = tag_hash.values.flatten
-    product_ids = Product.joins(:tags).where(tags: {id: tag_ids}).group(:product_id).count.keep_if{|k, v| v >= tag_hash.count}.keys
-    Product.includes(:tags).where(products: { id: product_ids}).uniq
+    product_ids = Product.joins(:tags).where(tags: {id: tag_ids}).group("products.id").select{|product| product.would_show_up_for(tag_hash)}.map(&:id)
+    Product.where(id: product_ids).includes(:tags)
+  end
+
+  def would_show_up_for(tag_hash)
+    category_check = true
+    tag_hash.each do |category_id, tag_ids|
+      if category_check == true
+        category_check = (tag_ids & self.tag_ids.map(&:to_s)).present?
+      end
+    end
+    category_check
+  end
+
+  def categories
+    tags.map(&:category)
   end
 
   private
